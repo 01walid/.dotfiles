4abf7ed6f819c7869ae4ff36e3cccb48c6ff7b4f
Walid Ziouche <01walid@users.noreply.github.com>
2022-09-07 23:15:39 +0100

    Rust inlay hint config updates for nvim

--- nvim/.config/nvim/lua/lsp-config/language-servers.lua
+++ nvim/.config/nvim/lua/lsp-config/language-servers.lua
@@ -47,17 +47,30 @@ for _, lsp in ipairs(servers) do
     })
 end
 
--- Custom Rust config
-local rt = require("rust-tools")
+local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-rt.setup({
+require('rust-tools').setup({
     server = {
-        on_attach = function(_, bufnr)
-            -- Hover actions
-            vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-            -- Code action groups
-            vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-        end,
-    },
+		capabilities = capabilities,
+		on_attach = on_attach,
+	}
 })
+
+require('rust-tools').inlay_hints.enable()
+
+-- local rt = require("rust-tools")
+-- rt.setup()
+
+-- rt.setup({
+--     server = {
+--         on_attach = function(_, bufnr)
+--             -- Hover actions
+--             vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+--             -- Code action groups
+--             vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+--         end,
+--     },
+-- })
+-- 
+-- rt.inlay_hints.enable()
 
--- nvim/.config/nvim/lua/packer-config/init.lua
+++ nvim/.config/nvim/lua/packer-config/init.lua
@@ -36,6 +36,9 @@ return require('packer').startup(function()
 
     use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
     use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+    use 'hrsh7th/cmp-nvim-lua'
+    use 'hrsh7th/cmp-nvim-lsp-signature-help'
+    use 'hrsh7th/cmp-path'
     use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
     use 'L3MON4D3/LuaSnip' -- Snippets plugin
     use 'onsails/lspkind.nvim' -- Icons for auto-completion popup menus
